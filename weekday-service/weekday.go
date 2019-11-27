e099a08b841fcc4d5a3519b436a7fb4603f3b63d
Fighter Hayabusa <fighterhayabusa@dirtylabcoat.org>
2019-11-27 16:48:42 +0100

    Add weekday service

--- weekday-service/weekday.go
+++ weekday-service/weekday.go
@@ -10,29 +10,16 @@ import (
 type server struct{}
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	dateTime := dateTimeAsString()
-    weekday := getWeekday() 
-	out := fmt.Sprintf("{\"dateTime\":\"%s %s\"}", dateTime, weekday)
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(out))
-}
-
-func dateTimeAsString() string {
-	currentTime := time.Now()
-	return currentTime.Format("2006-01-02 15:04:05")
-}
-
-func getWeekday() string {
-    return ""
+	w.Write([]byte(fmt.Sprintf("{\"weekday\":\"%s\"}", time.Now().Format("Monday"))))
 }
 
 func main() {
 	s := &server{}
-	http.Handle("/datetime", s)
+	http.Handle("/weekday", s)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
-
